716b56d734d5f03df1c48d177c387749b55ec309
learnworksmart <learnworksmart@gmail.com>
2020-10-02 20:48:43 +0800

    update

--- challenges-01/ec2.tf
+++ challenges-01/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webserver" {
   ami                     = "ami-0c8e97a27be37adfd"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.base_key_pair.key_name
-  vpc_security_group_ids = ["${aws_security_group.base_allow_default.id}"]
+  vpc_security_group_ids = [aws_security_group.base_allow_default.id]
 
   tags    = { 
     Name  = "webserver"
@@ -16,7 +16,7 @@ resource "aws_instance" "non_webserver" {
   ami                     = "ami-0c8e97a27be37adfd"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.base_key_pair.key_name
-  vpc_security_group_ids = ["${aws_security_group.base_allow_default.id}"]
+  vpc_security_group_ids = [aws_security_group.base_allow_default.id]
 
   tags    = { 
     Name  = "non-webserver"
